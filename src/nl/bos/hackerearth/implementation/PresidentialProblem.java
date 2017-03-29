09f20f1b41a20054dbffe08083dab9b4bc3a9b8e
HetBenkt <HetBenkt@gmail.com>
2017-03-29 17:07:00 +0200

    Made it a bit faster and added some timing output

--- src/nl/bos/hackerearth/implementation/PresidentialProblem.java
+++ src/nl/bos/hackerearth/implementation/PresidentialProblem.java
@@ -5,6 +5,7 @@ package nl.bos.hackerearth.implementation;
  */
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class PresidentialProblem {
@@ -18,39 +19,41 @@ public class PresidentialProblem {
         s.nextLine(); // = Carriage return <Enter>
 
         for (int i = 0; i < nrTestCases; i++) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
             StringTokenizer queries = new StringTokenizer(s.nextLine(), DELIM);
             int nrQueries = Integer.parseInt(queries.nextElement().toString());
+            System.out.println("1. " + sdf.format(new Date()));
             String text = s.nextLine();
 
+            System.out.println("2. " + sdf.format(new Date()));
             int[][] values = revalidateInput(s, nrQueries);
+            System.out.println("3. " + sdf.format(new Date()));
             for (int j = 0; j < nrQueries; j++) {
-                text = shiftToThePreviousAlphabet(text, values[j][0]-1, values[j][1]);
+                text = shiftToThePreviousAlphabet(text, values[j][0] - 1, values[j][1]);
             }
-            System.out.println(text.substring(0, text.length()-1)); //BUG as input lenght is different from output lenght in test-case!
+            System.out.println("4. " + sdf.format(new Date()));
+            System.out.println(text.substring(0, text.length() - 1)); //BUG as input lenght is different from output lenght in test-case!
+            System.out.println("5. " + sdf.format(new Date()));
         }
         s.close();
     }
 
     private static String shiftToThePreviousAlphabet(String text, int nrL, int nrR) {
-
-        StringBuilder result = new StringBuilder();
         char[] chars = text.toCharArray();
-        for (int i = 0; i < text.length(); i++) {
-            if(i >= nrL && i <nrR) {
-                char c = chars[i];
-                c--;
-                if(c == 96) //a
-                    c = 122; //z
-                chars[i] = c;
-            }
-            result.append(chars[i]);
+        for (int i = nrL; i < nrR; i++) {
+            char c = chars[i];
+            c--;
+            if (c == 96) //a
+                c = 122; //z
+            chars[i] = c;
         }
-        return result.toString();
+        return String.valueOf(chars);
     }
 
-    private static int[][] revalidateInput(Scanner s, int nrQueries){
+    private static int[][] revalidateInput(Scanner s, int nrQueries) {
         int[][] result = new int[nrQueries][2];
-        for (int j = nrQueries-1; j >= 0; j--) {
+        for (int j = nrQueries - 1; j >= 0; j--) {
             StringTokenizer valuesLR = new StringTokenizer(s.nextLine(), DELIM);
             int nrL = Integer.parseInt(valuesLR.nextElement().toString());
             int nrR = Integer.parseInt(valuesLR.nextElement().toString());
