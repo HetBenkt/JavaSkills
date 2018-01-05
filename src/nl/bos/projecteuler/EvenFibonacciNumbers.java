eb9afc3a8b0eddaa0d4e3668c9bbbbf63308b8c7
HetBenkt <HetBenkt@gmail.com>
2018-01-05 11:44:09 +0100

    Fixed the project for IntelliJ and commit of first projecteuler problem

--- src/nl/bos/projecteuler/EvenFibonacciNumbers.java
+++ src/nl/bos/projecteuler/EvenFibonacciNumbers.java
@@ -1,8 +1,34 @@
 package nl.bos.projecteuler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bosa on 28-11-2017.
  */
 public class EvenFibonacciNumbers {
+    public static final int MAX_VALUE = 4000000;
+    public static final int START_VALUE_1 = 1;
+    public static final int START_VALUE_2 = 2;
 
+    public static void main(String[] args) {
+        List<Integer> evenValues = new ArrayList<Integer>();
+
+        int value1 = START_VALUE_1;
+        int value2 = START_VALUE_2;
+        int term;
+
+        evenValues.add(new Integer(value2));
+
+        while((value1 + value2) <= MAX_VALUE) {
+            term = value1 + value2;
+            if( term % 2 == 0 ) {
+                evenValues.add(new Integer(term));
+            }
+            value1 = value2;
+            value2 = term;
+        }
+
+        System.out.println(evenValues.stream().mapToInt(e -> e.intValue()).sum());
+    }
 }
