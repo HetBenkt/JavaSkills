658d20837918171e8acb38c780e50d423ed53bb4
Antal Bos <antal.bos@amplexor.com>
2018-03-04 10:53:49 +0100

    First set of values are made useable from top to down; Now we'll see if a sequence is still possible and where it's not we fix it.

--- src/nl/bos/projecteuler/MaximumPathSumI.java
+++ src/nl/bos/projecteuler/MaximumPathSumI.java
@@ -7,6 +7,7 @@ import lombok.extern.java.Log;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
@@ -14,8 +15,12 @@ import java.util.stream.Stream;
 public class MaximumPathSumI {
     private static final int SIZE = 15;
     private static Item[][] tree = new Item[SIZE][SIZE];
+    private static int totalSum = 0;
+    private static int totalValues = 0;
+    private static int average;
 
     public static void main(String[] args) {
+        //read input file and fill the array
         Class maximumPathSumIClass = MaximumPathSumI.class;
         Path path = Paths.get("src//nl//bos//projecteuler//" + maximumPathSumIClass.getSimpleName() + ".in").toAbsolutePath();
         try (Stream<String> stream = Files.lines(path)) {
@@ -29,14 +34,46 @@ public class MaximumPathSumI {
         } catch (Exception e) {
             log.finest(e.getLocalizedMessage());
         }
+
+        //get the average
+        average = totalSum / totalValues;
+        log.info(MessageFormat.format("The average value is {0}", average));
+
+        //make all values in array usable that are > the average and next line values are > average
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (tree[i][j] != null) {
+                    Item item = tree[i][j];
+                    if ((item.getValue() > average) && nextLineValuesAreAboveAverage(i, j)) {
+                        item.setUseable(true);
+                    }
+                } else {
+                    j = SIZE;
+                }
+            }
+        }
+    }
+
+    private static boolean nextLineValuesAreAboveAverage(int i, int j) {
+        //Check last line; for arrayOutOfBounce!
+        if (i + 1 != SIZE) {
+            if (tree[i + 1][j].getValue() > average || tree[i + 1][j + 1].getValue() > average)
+                return true;
+            else
+                return false;
+        } else {
+            return true;
+        }
     }
 
     private static void addLineToTreeArray(String line, int lineNr) {
         StringTokenizer tokenizer = new StringTokenizer(line, " ");
         int tokenIndex = 0;
         while (tokenizer.hasMoreTokens()) {
-            String value = tokenizer.nextToken();
-            tree[lineNr][tokenIndex] = new Item(Integer.parseInt(value), false);
+            int value = Integer.parseInt(tokenizer.nextToken());
+            totalSum += value;
+            totalValues++;
+            tree[lineNr][tokenIndex] = new Item(value, false);
             tokenIndex++;
         }
     }
