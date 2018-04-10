1d90e9841c87f3c79905308652dc381f7ecada04
Antal Bos <antal.bos@amplexor.com>
2018-04-10 11:34:44 +0200

    First one for this week

---
+++ src/main/java/nl/bos/projecteuler/LexicographicPermutations.java
@@ -0,0 +1,28 @@
+package nl.bos.projecteuler;
+
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import lombok.extern.java.Log;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Log
+public class LexicographicPermutations {
+    @Test
+    public void execute() {
+        List<Integer> vals = Lists.newArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Collection<List<Integer>> orderPerm =
+                Collections2.orderedPermutations(vals);
+
+        int index = 1;
+        for (List<Integer> val : orderPerm) {
+            if(index == 1000000)
+                log.info(String.valueOf(val));
+            index++;
+        }
+    }
+}
