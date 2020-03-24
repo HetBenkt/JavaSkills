db5ce0689f1de6bc6435c75ec247067502ef6c7c
Antal Bos <HetBenkt@gmail.com>
2020-03-24 16:01:24 +0100

    EvenOrOdd

---
+++ Codewars/src/main/java/nl/bos/kyu8/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package nl.bos.kyu8;
+
+class EvenOrOdd {
+
+    String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
---
+++ Codewars/src/test/java/nl/bos/kyu8/EvenOrOddTest.java
@@ -0,0 +1,14 @@
+package nl.bos.kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EvenOrOddTest {
+    @Test
+    public void testEvenOrOdd() {
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+        assertEquals(evenOrOdd.evenOrOdd(6), "Even");
+        assertEquals(evenOrOdd.evenOrOdd(7), "Odd");
+    }
+}
