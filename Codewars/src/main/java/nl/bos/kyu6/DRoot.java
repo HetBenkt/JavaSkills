6bd29724eb1d6a5d85516c46fa265c11b4e99d11
Antal Bos <HetBenkt@gmail.com>
2020-04-20 17:04:30 +0200

    Init Sum of Digits / Digital Root

---
+++ Codewars/src/main/java/nl/bos/kyu6/DRoot.java
@@ -0,0 +1,7 @@
+package nl.bos.kyu6;
+
+public class DRoot {
+    public static int digital_root(int n) {
+        return 0;
+    }
+}
---
+++ Codewars/src/test/java/nl/bos/kyu6/DRootExampleTest.java
@@ -0,0 +1,13 @@
+package nl.bos.kyu6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DRootExampleTest {
+    @Test
+    public void Tests() {
+        assertEquals("Nope!", 7, DRoot.digital_root(16));
+        assertEquals("Nope!", 6, DRoot.digital_root(456));
+    }
+}
