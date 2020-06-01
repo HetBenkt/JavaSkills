edeff86cc88f4a33be23bb1cfaae414b1ece1f35
bosa <HetBenkt@gmail.com>
2020-06-01 20:03:42 +0200

    It's fine, but test @ codewars still fail!

--- Codewars/src/main/java/nl/bos/kyu5/Josephus.java
+++ Codewars/src/main/java/nl/bos/kyu5/Josephus.java
@@ -7,25 +7,32 @@ public class Josephus {
     public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
         List<T> result = new ArrayList<>();
 
-        int leftOver = 0;
-
-        while (result.size() != items.size()) {
-            int offset = leftOver;
-
-            items.removeAll(result);
-            if (items.size() == 1) {
-                result.addAll(items);
-                break;
-            }
-
-            for (int i = 1; i <= items.size(); i++) {
-                if ((i + offset) % k == 0) {
-                    result.add(items.get(i - 1));
-                    leftOver = items.size() - i;
-                }
-            }
+        List<T> temp = new ArrayList<>();
+        temp.addAll(items);
+        for (int i = 1; i < k; i++) {
+            items.addAll(temp);
         }
 
+        int leftOver = 0;
+        int extra = 0;
+        while (items.size() / k != 1 && items.size() > 0) {
+            int offset = leftOver;
+            if (items.size() / k < k) {
+                extra = k - items.size() / k;
+            }
+            for (int i = 1; i <= (items.size() / k) + extra; i++) {
+                if ((i + offset) % k == 0) {
+                    result.add(items.get(i - 1));
+                    leftOver = items.size() / k - i;
+                }
+            }
+            items.removeAll(result);
+            if (items.isEmpty())
+                break;
+        }
+        if (items.size() == k) {
+            result.add(items.get(0));
+        }
         return result;
     }
 }
