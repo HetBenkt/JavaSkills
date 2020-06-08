8c67fa91d1780766e429e9e93b4a66d2baee3640
bosa <HetBenkt@gmail.com>
2020-06-08 23:40:00 +0200

    It's fine (second try), but test @ codewars still fail! Looks like we can have duplicate "cloned" people...sure!!!

--- Codewars/src/main/java/nl/bos/kyu5/Josephus.java
+++ Codewars/src/main/java/nl/bos/kyu5/Josephus.java
@@ -9,30 +9,23 @@ public class Josephus {
 
         List<T> temp = new ArrayList<>();
         temp.addAll(items);
-        for (int i = 1; i < k; i++) {
+        for (int i = 1; i < k * 6; i++) {
             items.addAll(temp);
         }
 
-        int leftOver = 0;
-        int extra = 0;
-        while (items.size() / k != 1 && items.size() > 0) {
-            int offset = leftOver;
-            if (items.size() / k < k) {
-                extra = k - items.size() / k;
-            }
-            for (int i = 1; i <= (items.size() / k) + extra; i++) {
-                if ((i + offset) % k == 0) {
-                    result.add(items.get(i - 1));
-                    leftOver = items.size() / k - i;
+        int index = 1;
+        for (int i = 1; i <= items.size(); i++) {
+            T t = items.get(i - 1);
+            if (index % k == 0 && !result.contains(t)) {
+                result.add(t);
+                index++;
+            } else {
+                if (!result.contains(t)) {
+                    index++;
                 }
             }
-            items.removeAll(result);
-            if (items.isEmpty())
-                break;
         }
-        if (items.size() == k) {
-            result.add(items.get(0));
-        }
+
         return result;
     }
 }
