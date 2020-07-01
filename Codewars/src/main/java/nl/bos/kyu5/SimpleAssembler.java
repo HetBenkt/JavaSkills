f3344c6af86c60edc99de630e13dc0666995fa03
bosa <HetBenkt@gmail.com>
2020-07-01 15:21:07 +0200

    All simple tests OK

--- Codewars/src/main/java/nl/bos/kyu5/SimpleAssembler.java
+++ Codewars/src/main/java/nl/bos/kyu5/SimpleAssembler.java
@@ -7,7 +7,9 @@ public class SimpleAssembler {
     static Map<String, Integer> register = new HashMap<>();
 
     public static Map<String, Integer> interpret(String[] program) {
-        for (String instruction : program) {
+        for (int i = 0; i < program.length; i++) {
+            String instruction = program[i];
+
             switch (instruction.substring(0, 3)) {
                 case "mov":
                     doMove(instruction.substring(4));
@@ -19,31 +21,48 @@ public class SimpleAssembler {
                     doDec(instruction.substring(4));
                     break;
                 case "jnz":
-                    doJnz(instruction.substring(4));
+                    String[] split = instruction.substring(4).split(" ");
+                    String registerLocation = split[0];
+                    Integer registerJumpValue = Integer.parseInt(split[1]);
+                    if (register.get(registerLocation) == 0) {
+                        break;
+                    }
+                    if (registerJumpValue < 0) {
+                        i--;
+                    }
+                    i += registerJumpValue;
                     break;
                 default:
             }
+
+            register.forEach((key, value) -> System.out.println(key + ":" + value));
         }
 
         return register;
     }
 
-    private static void doJnz(String jnzInstruction) {
-
+    private static void doDec(String regLocation) {
+        Integer integer = register.get(regLocation);
+        integer--;
+        register.put(regLocation, integer);
     }
 
-    private static void doDec(String decInstruction) {
-
-    }
-
-    private static void doInc(String incInstruction) {
-
+    private static void doInc(String regLocation) {
+        Integer integer = register.get(regLocation);
+        integer++;
+        register.put(regLocation, integer);
     }
 
     private static void doMove(String moveInstruction) {
         String[] split = moveInstruction.split(" ");
         String registerLocation = split[0];
-        Integer registerValue = Integer.parseInt(split[1]);
-        register.put(registerLocation, registerValue);
+        String registerValue = split[1];
+        Integer value = 0;
+        try {
+            value = Integer.parseInt(registerValue);
+        } catch (NumberFormatException nfo) {
+            value = register.get(registerValue);
+        }
+        register.put(registerLocation, value);
     }
 }
