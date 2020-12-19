b101930191e4b56f20e22349534704b00ea69b2e
bosa <HetBenkt@gmail.com>
2020-12-19 17:07:58 +0100

    Day1ReportRepair

--- AdventOfCode/src/main/java/nl/bos/a2020/Day1ReportRepair.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day1ReportRepair.java
@@ -1,11 +1,14 @@
 package nl.bos.a2020;
 
+import org.paukov.combinatorics3.Generator;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Day1ReportRepair {
 
@@ -13,20 +16,25 @@ public class Day1ReportRepair {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day1ReportRepair");
         ArrayList<String> data = readData(is);
 
-        for (int i = 0; i < data.size(); i++) {
-            int value1 = Integer.parseInt(data.get(i));
-            for (int j = i + 1; j < data.size() - i - 1; j++) {
-                int value2 = Integer.parseInt(data.get(j));
-                if (value1 + value2 == 2020) {
-                    System.out.println(String.format("%d x %d = %d", value1, value2, value1 * value2));
-                    System.exit(0);
-                }
-            }
-        }
+        Generator.combination(data).simple(3).stream().forEach(this::sum);
     }
 
     public static void main(String[] args) {
         new Day1ReportRepair();
+    }
+
+    private void sum(List<String> combination) {
+        int sum = 0;
+        for (String value : combination) {
+            sum += Integer.parseInt(value);
+        }
+        if (sum == 2020) {
+            int value1 = Integer.parseInt(combination.get(0));
+            int value2 = Integer.parseInt(combination.get(1));
+            int value3 = Integer.parseInt(combination.get(2));
+            System.out.println(String.format("%d x %d x %d = %d", value1, value2, value3, value1 * value2 * value3));
+            System.exit(0);
+        }
     }
 
     private ArrayList<String> readData(InputStream is) {
