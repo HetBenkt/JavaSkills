236bea90ab0f4c63f6388b46ff6a3b80999c7047
bosa <HetBenkt@gmail.com>
2020-12-20 12:28:14 +0100

    Day3TobogganTrajectory - part1

--- AdventOfCode/src/main/java/nl/bos/a2020/Day3TobogganTrajectory.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day3TobogganTrajectory.java
@@ -6,12 +6,26 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 public class Day3TobogganTrajectory {
+    private final static int ROW_WIDTH = 31;
 
     public Day3TobogganTrajectory() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day3TobogganTrajectory");
         ArrayList<String> data = AdventReadInput.readData(is);
 
+        int column = 0;
+        int trees = 0;
+        for (int row = 0; row < data.size(); row++) {
+            char point = data.get(row).charAt(column);
+            if (point == '#') {
+                trees++;
+            }
+            column += 3;
+            if (column >= ROW_WIDTH) {
+                column = column - ROW_WIDTH;
+            }
+        }
 
+        System.out.println(String.format("Encountered trees = %d", trees));
     }
 
     public static void main(String[] args) {
