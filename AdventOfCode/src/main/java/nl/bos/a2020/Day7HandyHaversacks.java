8d7cd9bf22a220f7d6fca523f3b95449404805df
bosa <HetBenkt@gmail.com>
2021-01-07 15:12:24 +0100

    Day7HandyHaversacks - part1

--- AdventOfCode/src/main/java/nl/bos/a2020/Day7HandyHaversacks.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day7HandyHaversacks.java
@@ -4,36 +4,36 @@ import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-//468 too high
 public class Day7HandyHaversacks {
-
-    private int counter = 0;
 
     public Day7HandyHaversacks() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day7HandyHaversacks");
         List<String> data = AdventReadInput.readData(is);
 
         List<String> bagTypes = new ArrayList<>();
+        Set<String> uniqueBagTypes = new HashSet<>();
         bagTypes.add("shiny gold");
         while (bagTypes.size() > 0) {
             List<String> resultBagTypes = new ArrayList<>();
-
             for (String dataLine : data) {
                 String[] split = dataLine.split(" bags contain");
                 for (String bagType : bagTypes) {
                     if (split[1].contains(bagType)) {
-                        counter++;
                         resultBagTypes.add(split[0]);
                     }
                 }
             }
+            resultBagTypes.removeAll(uniqueBagTypes);
+            uniqueBagTypes.addAll(resultBagTypes);
             bagTypes.clear();
             bagTypes.addAll(resultBagTypes);
         }
 
-        System.out.println(counter);
+        System.out.println(String.format("%d bag colors can eventually contain at least one shiny gold bag!", uniqueBagTypes.size()));
     }
 
     public static void main(String[] args) {
