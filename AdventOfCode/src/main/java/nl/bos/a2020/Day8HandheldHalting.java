ff8727e65cccecb9e754ebe7254209024352a2b4
bosa <HetBenkt@gmail.com>
2021-02-05 16:28:15 +0100

    Part 1 is DONE!

--- AdventOfCode/src/main/java/nl/bos/a2020/Day8HandheldHalting.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day8HandheldHalting.java
@@ -3,15 +3,46 @@ package nl.bos.a2020;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day8HandheldHalting {
 
     public Day8HandheldHalting() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day8HandheldHalting");
-        List<String> data = AdventReadInput.readData(is);
+        List<String> bootCode = AdventReadInput.readData(is);
 
-        System.out.println(String.format("%d individual bags are required inside the single shiny gold bag!", 0));
+        boolean run = true;
+        int accumulator = 0;
+        int index = 0;
+        List<Integer> indexes = new ArrayList<>();
+
+        while (run) {
+            String[] split = bootCode.get(index).split(" ");
+            String operation = split[0];
+            int argument = Integer.parseInt(split[1]);
+            switch (operation) {
+                case "acc":
+                    accumulator += argument;
+                    index++;
+                    break;
+                case "jmp":
+                    index += argument;
+                    break;
+                case "nop":
+                    index++;
+                    break;
+                default:
+                    break;
+            }
+            if (!indexes.contains(index)) {
+                indexes.add(index);
+            } else {
+                run = false;
+            }
+        }
+
+        System.out.println(String.format("Value of the accumulator is: %d", accumulator));
     }
 
     public static void main(String[] args) {
