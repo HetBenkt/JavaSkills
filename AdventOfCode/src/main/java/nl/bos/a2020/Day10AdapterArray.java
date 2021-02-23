de6c416ef0ff9e0b8f4ae52077fbae470dc27ef8
bosa <HetBenkt@gmail.com>
2021-02-23 23:41:07 +0100

    Part 2

--- AdventOfCode/src/main/java/nl/bos/a2020/Day10AdapterArray.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day10AdapterArray.java
@@ -3,6 +3,7 @@ package nl.bos.a2020;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,21 +16,55 @@ public class Day10AdapterArray {
 
         List<Integer> integers = data.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
         Collections.sort(integers);
+        List<Integer> jumpNumbers = convertToJumpNumbers(integers);
+        jumpNumbers.add(3);//For your own built-in jolt adapter!
 
-        int previousValue = 0;
-        int oneJoltDifferences = 0;
-        int threeJoltDifferences = 1; //Start with 1 for your own built-in joltage adapter!
-        for (int value : integers) {
-            if (value - previousValue == 1) {
-                oneJoltDifferences++;
+        int jumpSequence = 0;
+        long result = 1;
+        for (int jumpNumber : jumpNumbers) {
+            if (jumpNumber == 3) {
+                result *= getTribonacci(jumpSequence); //https://mathworld.wolfram.com/TribonacciNumber.html
+                jumpSequence = 0;
+                continue;
             }
-            if (value - previousValue == 3) {
-                threeJoltDifferences++;
-            }
-            previousValue = value;
+            jumpSequence++;
         }
 
-        System.out.print(String.format("%d * %d = %d", oneJoltDifferences, threeJoltDifferences, oneJoltDifferences * threeJoltDifferences));
+
+        System.out.print(String.format("Result = %d", result));
+    }
+
+    private int getTribonacci(int jumpNumber) {
+        if (jumpNumber == 0 || jumpNumber == 1) {
+            return 1;
+        } else if (jumpNumber == 2) {
+            return 2;
+        } else if (jumpNumber == 3) {
+            return 4;
+        } else if (jumpNumber == 4) {
+            return 7;
+        } else if (jumpNumber == 5) {
+            return 13;
+        } else if (jumpNumber == 6) {
+            return 24;
+        } else if (jumpNumber == 7) {
+            return 44;
+        } else if (jumpNumber == 8) {
+            return 81;
+        } else if (jumpNumber == 9) {
+            return 149;
+        }
+        return 0;
+    }
+
+    private List<Integer> convertToJumpNumbers(List<Integer> integers) {
+        List<Integer> result = new ArrayList<>();
+        int previousValue = 0;
+        for (int value : integers) {
+            result.add(value - previousValue);
+            previousValue = value;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
