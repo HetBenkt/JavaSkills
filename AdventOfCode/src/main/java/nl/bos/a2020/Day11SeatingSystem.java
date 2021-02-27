1b28aa11236a5b6b451d8180fcd68a422bcd32c7
bosa <HetBenkt@gmail.com>
2021-02-27 21:46:05 +0100

    Creating a grid

--- AdventOfCode/src/main/java/nl/bos/a2020/Day11SeatingSystem.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day11SeatingSystem.java
@@ -11,7 +11,20 @@ public class Day11SeatingSystem {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day11SeatingSystem");
         List<String> data = AdventReadInput.readData(is);
 
-        //TODO
+        int cols = data.get(0).length() + 1;
+        int rows = data.size() + 1;
+        char[][] grid = new char[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (i == 0 || i == cols - 1) {
+                    grid[i][j] = '.';
+                } else if (j == 0 || j == rows - 1) {
+                    grid[i][j] = '.';
+                } else {
+                    grid[i][j] = '#';
+                }
+            }
+        }
 
         System.out.print(String.format("Result = %d", 0));
     }
