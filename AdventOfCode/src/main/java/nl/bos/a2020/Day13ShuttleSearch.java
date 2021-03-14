a7df004205e37b103c053d7973b03cd4e7520dbd
bosa <HetBenkt@gmail.com>
2021-03-14 10:47:34 +0100

    Part 1 is OK

--- AdventOfCode/src/main/java/nl/bos/a2020/Day13ShuttleSearch.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day13ShuttleSearch.java
@@ -3,7 +3,10 @@ package nl.bos.a2020;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day13ShuttleSearch {
 
@@ -14,7 +17,26 @@ public class Day13ShuttleSearch {
         int timestamp = Integer.parseInt(data.get(0)); //in minutes
         String[] inServiceBusIds = data.get(1).split(",");
 
-        System.out.println(String.format("ID of the earliest bus = %d", 0));
+        Map<Integer, Integer> closestTimestamps = new HashMap<>();
+        for (String inServiceBusId : inServiceBusIds) {
+            if (!inServiceBusId.equals("x")) {
+                int busId = Integer.parseInt(inServiceBusId);
+                int rest = timestamp % busId;
+                closestTimestamps.put(busId, (timestamp - rest) + busId);
+            }
+        }
+
+        int lowestTimestamp = Collections.min(closestTimestamps.values());
+        int busId = 0;
+        for (Map.Entry<Integer, Integer> entry : closestTimestamps.entrySet()) {
+            busId = entry.getKey();
+            int value = entry.getValue();
+            if (value == lowestTimestamp) {
+                break;
+            }
+        }
+
+        System.out.println(String.format("ID of the earliest bus = %d", (lowestTimestamp - timestamp) * busId));
     }
 
     public static void main(String[] args) {
