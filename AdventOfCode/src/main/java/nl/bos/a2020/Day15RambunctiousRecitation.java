ccf12a61d318efadb4340f96527b5f28583915c7
bosa <HetBenkt@gmail.com>
2021-03-28 11:19:59 +0200

    Part 1...OK

--- AdventOfCode/src/main/java/nl/bos/a2020/Day15RambunctiousRecitation.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day15RambunctiousRecitation.java
@@ -3,17 +3,49 @@ package nl.bos.a2020;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day15RambunctiousRecitation {
+
+    public static final int YEAR = 2020;
 
     public Day15RambunctiousRecitation() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day15RambunctiousRecitation");
         List<String> data = AdventReadInput.readData(is);
 
-        //TODO
+        List<Long> startNumbers = Stream.of(data.get(0).split(",")).map(Long::valueOf).collect(Collectors.toList());
+        List<Long> numbers = new ArrayList<>();
+        numbers.addAll(startNumbers);
 
-        System.out.println(String.format("The 2020th number spoken = %d", 0));
+        int turn = numbers.size();
+        while (turn < YEAR) {
+            long lastSpokenNr = numbers.get(numbers.size() - 1);
+            long count = numbers.stream().filter(c -> c == lastSpokenNr).count();
+            if (count == 1) {
+                numbers.add(0L);
+            } else if (count >= 2) {
+                List<Integer> indexes = getIndexes(numbers, lastSpokenNr);
+                numbers.add((long) ((indexes.get(indexes.size() - 1) + 1) - (indexes.get(indexes.size() - 2) + 1)));
+            }
+            turn++;
+        }
+
+        System.out.println(String.format("The 2020th number spoken = %d", numbers.get(YEAR - 1)));
+    }
+
+    private List<Integer> getIndexes(List<Long> numbers, long lastSpokenNr) {
+        List<Integer> matchingIndices = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            long element = numbers.get(i);
+
+            if (lastSpokenNr == element) {
+                matchingIndices.add(i);
+            }
+        }
+        return matchingIndices;
     }
 
     public static void main(String[] args) {
