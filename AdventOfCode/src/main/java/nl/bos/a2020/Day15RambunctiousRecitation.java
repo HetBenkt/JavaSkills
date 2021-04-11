1472ab6a0e6e26d690278ab78ec644bdac14e2bf
bosa <HetBenkt@gmail.com>
2021-04-11 21:36:12 +0200

    FINALLY...with a little help from: https://raw.githubusercontent.com/funand/AdventOfCode-RambunctiousRecitation-TurnNumber/29872b123bb2e2c0c8ca939f36d70ccd20677f5b/src/TurnNumbers.java

--- AdventOfCode/src/main/java/nl/bos/a2020/Day15RambunctiousRecitation.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day15RambunctiousRecitation.java
@@ -4,81 +4,85 @@ import nl.bos.general.AdventReadInput;
 import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.InputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day15RambunctiousRecitation {
 
-    public static final int YEAR = 30000000; //30000000
+    public static final int YEAR = 30000000;
 
     public Day15RambunctiousRecitation() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day15RambunctiousRecitation");
         List<String> data = AdventReadInput.readData(is);
 
-        List<Long> startNumbers = Stream.of(data.get(0).split(",")).map(Long::valueOf).collect(Collectors.toList());
-        List<Long> numbers = new ArrayList<>();
-        numbers.addAll(startNumbers);
+        List<Long> numbers = Stream.of(data.get(0).split(",")).map(Long::valueOf).collect(Collectors.toList());
 
-        Map<Long, Long> countMap = new HashMap<>();
+        HashMap<Long, PlayedNumber> locationMap = new HashMap<>();
+        int index = 0;
         for (long number : numbers) {
-            countMap.put(number, (countMap.get(number) == null) ? 1L : countMap.get(number) + 1L);
+            locationMap.put(number, new PlayedNumber(index, -1));
+            index++;
         }
 
         int turn = numbers.size();
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         while (turn < YEAR) {
-            long lastSpokenNr = numbers.get(numbers.size() - 1);
-            long count = countMap.get(lastSpokenNr);
-            //long count = numbers.parallelStream().filter(c -> c == lastSpokenNr).count();
-            //long count = numbers.stream().filter(c -> c == lastSpokenNr).count();
-            //long count = getCount(numbers, lastSpokenNr);
-            if (count == 1) {
+            long previousNumber = numbers.get(turn - 1);
+
+            if (locationMap.containsKey(previousNumber) && locationMap.get(previousNumber).getPreviousLast() != -1) {
+                // contains previousNumber
+                long diff = locationMap.get(previousNumber).getLast() - locationMap.get(previousNumber).getPreviousLast();
+                numbers.add(diff);
+                if (locationMap.containsKey(diff)) {
+                    // contains difference, last - previous last number
+                    locationMap.put(diff, locationMap.get(diff).updatePositions(turn));
+                } else {
+                    locationMap.put(diff, new PlayedNumber(turn, -1));
+                }
+            } else {
+                // doesn't contain previousNumber, add
                 numbers.add(0L);
-                countMap.put(0L, (countMap.get(0L) == null) ? 1L : countMap.get(0L) + 1L);
-            } else if (count >= 2) {
-                List<Integer> indexes = getIndexes(numbers, lastSpokenNr);
-                long number = ((indexes.get(indexes.size() - 2) + 1) - (indexes.get(indexes.size() - 1) + 1));
-                numbers.add(number);
-                countMap.put(number, (countMap.get(number) == null) ? 1L : countMap.get(number) + 1L);
+                locationMap.put(0L, locationMap.get(0L).updatePositions(turn));
             }
             turn++;
-            if (turn % 100000 == 0) {
-                System.out.println(String.format("%s sec. [turn %s]", stopWatch.getTime() / 1000, turn));
+            if (turn % 1000000 == 0) {
+                System.out.printf("%s sec. [turn %s]%n", stopWatch.getTime() / 1000, turn);
                 stopWatch.reset();
                 stopWatch.start();
             }
         }
-        //System.out.println(String.format("%s", stopWatch.getTime()));
         stopWatch.stop();
-        System.out.println(String.format("The number spoken = %d", numbers.get(YEAR - 1)));
-    }
-
-    private long getCount(List<Long> numbers, long lastSpokenNr) {
-        List<Long> numbersTemp = new ArrayList<>();
-        numbersTemp.addAll(numbers);
-        Collections.sort(numbersTemp);
-        long first = numbersTemp.indexOf(lastSpokenNr);
-        long last = numbersTemp.lastIndexOf(lastSpokenNr);
-        return (last - first) + 1;
-    }
-
-    private List<Integer> getIndexes(List<Long> numbers, long lastSpokenNr) {
-        List<Integer> matchingIndices = new ArrayList<>();
-
-        int lastIndexOf = numbers.lastIndexOf(lastSpokenNr);
-        matchingIndices.add(lastIndexOf);
-        numbers.set(lastIndexOf, -1L);
-
-        int lastIndexOf1 = numbers.lastIndexOf(lastSpokenNr);
-        matchingIndices.add(lastIndexOf1);
-        numbers.set(lastIndexOf, lastSpokenNr);
-
-        return matchingIndices;
+        System.out.printf("The number spoken = %d%n", numbers.get(YEAR - 1));
     }
 
     public static void main(String[] args) {
         new Day15RambunctiousRecitation();
+    }
+
+    private static class PlayedNumber {
+        private int last;
+        private int previousLast;
+
+        private PlayedNumber(int last, int previousLast) {
+            this.last = last;
+            this.previousLast = previousLast;
+        }
+
+        private PlayedNumber updatePositions(int newLast) {
+            previousLast = last;
+            last = newLast;
+            return new PlayedNumber(last, previousLast);
+        }
+
+        public int getLast() {
+            return last;
+        }
+
+        public int getPreviousLast() {
+            return previousLast;
+        }
     }
 }
