675ed1f1a207de84a95397f6c1eea749337da9b3
bosa <HetBenkt@gmail.com>
2021-05-07 21:29:18 +0200

    Init

---
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day18OperationOrder.java
@@ -0,0 +1,19 @@
+package nl.bos.a2020;
+
+import nl.bos.general.AdventReadInput;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class Day18OperationOrder {
+
+    public Day18OperationOrder() {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day18OperationOrder");
+        List<String> data = AdventReadInput.readData(is);
+
+        //TODO
+
+        System.out.printf("The sum of the resulting values = %d%n", 0);
+    }
+}
+
