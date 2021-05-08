afdb8d581398cade0f5c81f7bff901c6a53d7645
bosa <HetBenkt@gmail.com>
2021-05-09 00:01:11 +0200

    a Rewrite works better...Now to a list

--- AdventOfCode/src/main/java/nl/bos/a2020/Day18OperationOrder.java
+++ AdventOfCode/src/main/java/nl/bos/a2020/Day18OperationOrder.java
@@ -5,17 +5,26 @@ import nl.bos.general.AdventReadInput;
 import javax.script.ScriptException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day18OperationOrder {
 
-    public Day18OperationOrder() throws ScriptException {
+    public Day18OperationOrder() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2020/Day18OperationOrder");
         List<String> expressions = AdventReadInput.readData(is);
 
         List<Long> resultValues = new ArrayList<>();
         for (String expression : expressions) {
             String rewrite = new Rewriter().rewrite(expression);
+            String[] strings = rewrite.split("\n");
+            List<String> list = Arrays.asList(strings);
+            list.forEach(s -> {
+                if (s.startsWith("--")) {
+                    s = "-" + calc(s.substring(3));
+                }
+            });
+
             long resultValue = calc(expression);
             System.out.printf("resultValue = %d%n", resultValue);
             resultValues.add(resultValue);
@@ -28,16 +37,16 @@ public class Day18OperationOrder {
         new Day18OperationOrder();
     }
 
-    private long calc(String expression) throws ScriptException {
-        String[] chars = expression.split("[\\(||//)]");
+    private long calc(String expression) {
+        String[] chars = expression.split(" ");
         long totalValue = 0L;
         for (String c : chars) {
             if (c.matches("\\d+")) {
+                //TODO
             }
 
         }
-
-        return 0;
+        return totalValue;
     }
 
     private class Rewriter {
@@ -57,9 +66,6 @@ public class Day18OperationOrder {
                         break;
                     case ',':
                         appendNewLine();
-                        break;
-                    case ' ':
-                        // trim property names by skipping spaces.
                         break;
                     default:
                         output.append(character);
