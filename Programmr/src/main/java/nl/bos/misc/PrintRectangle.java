49c392a4184afff99550450c71b92afb8e274b36
bosa <HetBenkt@gmail.com>
2021-05-17 17:35:18 +0200

    PrintRectangle, but should be rotated

---
+++ Programmr/src/main/java/nl/bos/misc/PrintRectangle.java
@@ -0,0 +1,36 @@
+package nl.bos.misc;
+
+import java.util.Scanner;
+
+public class PrintRectangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the height:");
+        int height = scan.nextInt();
+        System.out.println("Enter the width:");
+        int width = scan.nextInt();
+
+        String[][] rect = buildRect(height, width);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(rect[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] buildRect(int height, int width) {
+        String[][] rect = new String[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                rect[i][j] = " ";
+                if (i == 0 || i == height - 1) {
+                    rect[i][j] = "*";
+                } else if (j == 0 || j == width - 1) {
+                    rect[i][j] = "*";
+                }
+            }
+        }
+        return rect;
+    }
+}
---
+++ Programmr/src/test/java/nl/bos/misc/PrintRectangleTest.java
@@ -0,0 +1,25 @@
+package nl.bos.misc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrintRectangleTest {
+
+    @Test
+    public void buildRect() {
+        //given
+        int width = 10;
+        int height = 5;
+        String[][] expecteds = {
+                {"*", "*", "*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", " ", " ", " ", " ", " ", " ", " ", " ", "*"},
+                {"*", " ", " ", " ", " ", " ", " ", " ", " ", "*"},
+                {"*", " ", " ", " ", " ", " ", " ", " ", " ", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*", "*", "*"}
+        };
+        //when
+        String[][] actuals = PrintRectangle.buildRect(width, height);
+        //then
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+}
