451bcf35fa31f9fac10fda5e53e9ab7849a6ac6c
hetbenkt@gmail.com <Abos@-02>
2021-06-02 10:17:29 +0200

    Made the code a bit nicer

--- Programmr/src/main/java/nl/bos/misc/PascalTriangle.java
+++ Programmr/src/main/java/nl/bos/misc/PascalTriangle.java
@@ -1,6 +1,31 @@
 package nl.bos.misc;
 
+import java.util.Scanner;
+
 public class PascalTriangle {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter number of rows for pascal triangle:");
+        int input = scan.nextInt();
+        System.out.printf("Pascal Triangle of %s is:", input);
+        System.out.println();
+
+        int width = input * 2;
+        int height = input;
+        String[][] result = buildTriangle(width, height);
+        print(result);
+    }
+
+    private static void print(String[][] result) {
+        for (String[] row : result) {
+            for (String rowCell : row) {
+                System.out.print(rowCell);
+            }
+            System.out.println();
+        }
+    }
+
     public static String[][] buildTriangle(int width, int height) {
         String[][] result = new String[height][width];
 
@@ -10,23 +35,23 @@ public class PascalTriangle {
                     result[i][j] = "";
                 } else {
                     result[i][j] = "_";
-                    if (i == 0 && (j == (width / 2) - 1)) {
+                    if (i == 0 && (j == height - 1)) {
                         result[i][j] = "1";
-                    } else if (i == 1 && (j == (width / 2) - 2 || j == width / 2)) {
+                    } else if (i == 1 && (j == height - 2 || j == height)) {
                         result[i][j] = "1";
-                    } else if (i == 2 && (j == (width / 2) - 1)) {
+                    } else if (i == 2 && (j == height - 1)) {
                         result[i][j] = "2";
-                    } else if (i == 2 && (j == (width / 2) - 3 || j == width / 2 + 1)) {
+                    } else if (i == 2 && (j == height - 3 || j == height + 1)) {
                         result[i][j] = "1";
-                    } else if (i == 3 && (j == (width / 2) - 2 || j == width / 2)) {
+                    } else if (i == 3 && (j == height - 2 || j == height)) {
                         result[i][j] = "3";
-                    } else if (i == 3 && (j == (width / 2) - 4 || j == width / 2 + 2)) {
+                    } else if (i == 3 && (j == height - 4 || j == height + 2)) {
                         result[i][j] = "1";
-                    } else if (i == 4 && (j == (width / 2) - 1)) {
+                    } else if (i == 4 && (j == height - 1)) {
                         result[i][j] = "6";
-                    } else if (i == 4 && (j == (width / 2) - 3 || j == width / 2 + 1)) {
+                    } else if (i == 4 && (j == height - 3 || j == height + 1)) {
                         result[i][j] = "4";
-                    } else if (i == 4 && (j == (width / 2) - 5 || j == width - 2)) {
+                    } else if (i == 4 && (j == height - 5 || j == height + 3)) {
                         result[i][j] = "1";
                     }
                 }
