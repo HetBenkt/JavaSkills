1054cd0de9d03fc340b69ef9315f38576b95d31e
hetbenkt@gmail.com <Abos@-02>
2021-12-01 22:22:41 +0100

    Part 1 is a go!

--- AdventOfCode/src/main/java/nl/bos/a2021/Day1SonarSweep.java
+++ AdventOfCode/src/main/java/nl/bos/a2021/Day1SonarSweep.java
@@ -9,9 +9,19 @@ public class Day1SonarSweep {
 
     public Day1SonarSweep() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2021/Day1SonarSweep");
-        ArrayList<String> data = AdventReadInput.readData(is);
+        ArrayList<String> sonarSweepReport = AdventReadInput.readData(is);
 
-        //todo
+        int result = 0;
+        int currentDepthMeasurement = Integer.parseInt(sonarSweepReport.get(0));
+        for (int i = 1; i < sonarSweepReport.size(); i++) {
+            int nextDepthMeasurement = Integer.parseInt(sonarSweepReport.get(i));
+            if (nextDepthMeasurement > currentDepthMeasurement) {
+                result++;
+            }
+            currentDepthMeasurement = nextDepthMeasurement;
+        }
+
+        System.out.println(String.format("Measurements are larger than the previous measurement: %s", result));
     }
 
     public static void main(String[] args) {
