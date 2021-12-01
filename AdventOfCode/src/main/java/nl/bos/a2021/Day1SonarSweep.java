ea2ddef14a8d6d38d7de78949ad73b1e39350984
hetbenkt@gmail.com <Abos@-02>
2021-12-01 22:40:10 +0100

    Part 2 is a go!

--- AdventOfCode/src/main/java/nl/bos/a2021/Day1SonarSweep.java
+++ AdventOfCode/src/main/java/nl/bos/a2021/Day1SonarSweep.java
@@ -4,17 +4,27 @@ import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Day1SonarSweep {
 
     public Day1SonarSweep() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2021/Day1SonarSweep");
-        ArrayList<String> sonarSweepReport = AdventReadInput.readData(is);
+        List<String> sonarSweepReport = AdventReadInput.readData(is);
 
         int result = 0;
-        int currentDepthMeasurement = Integer.parseInt(sonarSweepReport.get(0));
-        for (int i = 1; i < sonarSweepReport.size(); i++) {
-            int nextDepthMeasurement = Integer.parseInt(sonarSweepReport.get(i));
+
+        List<Integer> combinesSweepReport = new ArrayList<>();
+        for (int j = 0; j < sonarSweepReport.size() - 2; j++) {
+            int measure1 = Integer.parseInt(sonarSweepReport.get(j + 0));
+            int measure2 = Integer.parseInt(sonarSweepReport.get(j + 1));
+            int measure3 = Integer.parseInt(sonarSweepReport.get(j + 2));
+            combinesSweepReport.add(measure1 + measure2 + measure3);
+        }
+
+        int currentDepthMeasurement = combinesSweepReport.get(0);
+        for (int i = 1; i < combinesSweepReport.size(); i++) {
+            int nextDepthMeasurement = combinesSweepReport.get(i);
             if (nextDepthMeasurement > currentDepthMeasurement) {
                 result++;
             }
