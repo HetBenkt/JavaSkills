94cb1e6e553d315c7f5dbf8a393e6d8b98cbb558
hetbenkt@gmail.com <Abos@-02>
2021-12-05 23:46:01 +0100

    Part 1 is a go!

--- AdventOfCode/src/main/java/nl/bos/a2021/Day4GiantSquid.java
+++ AdventOfCode/src/main/java/nl/bos/a2021/Day4GiantSquid.java
@@ -3,6 +3,8 @@ package nl.bos.a2021;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day4GiantSquid {
@@ -11,12 +13,73 @@ public class Day4GiantSquid {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2021/Day4GiantSquid");
         List<String> data = AdventReadInput.readData(is);
 
-        //todo
+        //Prepare data
+        int[] bingoNumbers = Arrays.stream(data.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
+        List<BingoCard> bingoCards = new ArrayList<>();
+        for (int i = 1; i < data.size(); i += 6) {
+            int[][] cardNumbers = new int[5][5];
+            cardNumbers[0] = Arrays.stream(data.get(i + 1).split("(?<=\\G.{3})")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+            cardNumbers[1] = Arrays.stream(data.get(i + 2).split("(?<=\\G.{3})")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+            cardNumbers[2] = Arrays.stream(data.get(i + 3).split("(?<=\\G.{3})")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+            cardNumbers[3] = Arrays.stream(data.get(i + 4).split("(?<=\\G.{3})")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+            cardNumbers[4] = Arrays.stream(data.get(i + 5).split("(?<=\\G.{3})")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+            bingoCards.add(new BingoCard(cardNumbers));
+        }
 
-        System.out.printf("Final score be: %s", 0);
+        for (int bingoNumber : bingoNumbers) {
+            for (BingoCard bingoCard : bingoCards) {
+                int[][] card = bingoCard.numbers();
+                for (int row = 0; row < card.length; row++) {
+                    for (int col = 0; col < card[row].length; col++) {
+                        if (card[row][col] == bingoNumber) {
+                            int lastNumber = card[row][col];
+                            card[row][col] = -1;
+                            int result = doBingoCheck(bingoCards);
+                            if (result > 0) {
+                                System.out.printf("Final score be: %s", (result * lastNumber));
+                                System.exit(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private int doBingoCheck(List<BingoCard> bingoCards) {
+        for (BingoCard bingoCard : bingoCards) {
+            int[][] card = bingoCard.numbers();
+            for (int[] ints : card) {
+                if (Arrays.stream(ints).sum() == -5) {
+                    //found a bingo in row
+                    return Arrays.stream(card).flatMapToInt(Arrays::stream).filter(value -> value != -1).sum();
+                }
+            }
+        }
+
+        for (BingoCard bingoCard : bingoCards) {
+            int[][] card = bingoCard.numbers();
+            int[][] rotatedCard = new int[5][5];
+            for (int i = 0; i < card[0].length; i++) {
+                for (int j = card.length - 1; j >= 0; j--) {
+                    rotatedCard[i][j] = card[j][i];
+                }
+            }
+
+            for (int[] ints : rotatedCard) {
+                if (Arrays.stream(ints).sum() == -5) {
+                    //found a bingo in column
+                    return Arrays.stream(rotatedCard).flatMapToInt(Arrays::stream).filter(value -> value != -1).sum();
+                }
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
         new Day4GiantSquid();
+    }
+
+    public record BingoCard(int[][] numbers) {
     }
 }
