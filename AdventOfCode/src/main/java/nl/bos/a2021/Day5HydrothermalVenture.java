11860a33e6f25bf0338d944eca077caa647f368c
hetbenkt@gmail.com <Abos@-02>
2021-12-06 23:07:53 +0100

    Prepare data

--- AdventOfCode/src/main/java/nl/bos/a2021/Day5HydrothermalVenture.java
+++ AdventOfCode/src/main/java/nl/bos/a2021/Day5HydrothermalVenture.java
@@ -3,6 +3,7 @@ package nl.bos.a2021;
 import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day5HydrothermalVenture {
@@ -11,7 +12,18 @@ public class Day5HydrothermalVenture {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2021/Day5HydrothermalVenture");
         List<String> data = AdventReadInput.readData(is);
 
-        //todo
+        //Prepare data
+        List<Line> lines = new ArrayList<>();
+        for (String row : data) {
+            String[] split = row.split(" -> ");
+            String[] left = split[0].split(",");
+            String[] right = split[1].split(",");
+            lines.add(new Line(Integer.parseInt(left[0]),
+                    Integer.parseInt(left[1]),
+                    Integer.parseInt(right[0]),
+                    Integer.parseInt(right[1])
+            ));
+        }
 
         System.out.printf("At how many points do at least two lines overlap: %s", 0);
     }
@@ -19,5 +31,15 @@ public class Day5HydrothermalVenture {
 
     public static void main(String[] args) {
         new Day5HydrothermalVenture();
+    }
+
+    public enum EDirection {
+        VERTICAL, HORIZONTAL
+    }
+
+    public record Line(int x1, int y1, int x2, int y2, EDirection direction) {
+        public Line(int x1, int y1, int x2, int y2) {
+            this(x1, y1, x2, y2, (x1 == x2 ? EDirection.VERTICAL : EDirection.HORIZONTAL));
+        }
     }
 }
