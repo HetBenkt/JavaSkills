d65478b74e1dd3ea0a25b136832d81847fa0787e
hetbenkt@gmail.com <Abos@-02>
2021-12-11 00:37:03 +0100

    Part 1 is a go!

--- AdventOfCode/src/main/java/nl/bos/a2021/Day7TheTreacheryOfWhales.java
+++ AdventOfCode/src/main/java/nl/bos/a2021/Day7TheTreacheryOfWhales.java
@@ -4,6 +4,7 @@ import nl.bos.general.AdventReadInput;
 
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,9 +14,22 @@ public class Day7TheTreacheryOfWhales {
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2021/Day7TheTreacheryOfWhales");
         List<Integer> horizontalPositions = Arrays.stream(AdventReadInput.readData(is).get(0).split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
 
-        //todo
+        int fuelSpend = Integer.MAX_VALUE;
+        int index = 0;
+        while (index < Collections.max(horizontalPositions)) {
+            int finalIndex = index;
+            Integer sum = horizontalPositions.stream().map(integer -> {
+                integer = integer - finalIndex;
+                return Math.abs(integer);
+            }).mapToInt(Integer::intValue).sum();
 
-        System.out.printf("Fluel spend: %s", 0);
+            if (sum < fuelSpend) {
+                fuelSpend = sum;
+            }
+            index++;
+        }
+
+        System.out.printf("Fuel spend: %s", fuelSpend);
     }
 
     public static void main(String[] args) {
