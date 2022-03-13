8c6665398f1d8131a71b48beb0d3c412a0a0cd9f
hetbenkt@gmail.com <Abos@-02>
2022-03-13 10:32:22 +0100

    day3 - part1

--- AdventOfCode/src/main/java/nl/bos/a2019/Day3CrossedWires.java
+++ AdventOfCode/src/main/java/nl/bos/a2019/Day3CrossedWires.java
@@ -2,29 +2,28 @@ package nl.bos.a2019;
 
 import nl.bos.general.AdventReadInput;
 
+import java.awt.*;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-//13647 is too high!
 public class Day3CrossedWires {
 
-    private static final int GRID_SIZE = 500;
+    private static final int GRID_SIZE = 20_000;
     private final char[][] grid = new char[GRID_SIZE][GRID_SIZE];
 
-    private final int x1 = GRID_SIZE / 2;
-    private final int y1 = GRID_SIZE / 2;
-    private int x2 = GRID_SIZE / 2;
-    private int y2 = GRID_SIZE / 2;
-    private int xf2;
-    private int yf2;
-
+    private static final int X1 = GRID_SIZE / 2;
+    private static final int Y1 = GRID_SIZE / 2;
+    private final List<Point> crossSections = new ArrayList<>();
+    private int x2 = X1;
+    private int y2 = Y1;
 
     public Day3CrossedWires() {
         for (char[] row : grid) {
             Arrays.fill(row, '.');
         }
-        grid[x1][y1] = 'o';
+        grid[X1][Y1] = 'o';
 
         InputStream is = getClass().getClassLoader().getResourceAsStream("nl/bos/a2019/Day3CrossedWires");
         ArrayList<String> wires = AdventReadInput.readData(is);
@@ -37,8 +36,20 @@ public class Day3CrossedWires {
         y2 = GRID_SIZE / 2;
         drawOnGrid(wireTwo, false);
 
-        int manhattanDistance = Math.abs(x1 - xf2) + Math.abs(y1 - yf2);
-        System.out.println("Manhattan distance: " + manhattanDistance);
+        int xf2;
+        int yf2;
+        int result = GRID_SIZE;
+
+        for (Point crossSection : crossSections) {
+            xf2 = (int) crossSection.getX();
+            yf2 = (int) crossSection.getY();
+            int manhattanDistance = Math.abs(X1 - xf2) + Math.abs(Y1 - yf2);
+            if (result > manhattanDistance) {
+                result = manhattanDistance;
+            }
+        }
+
+        System.out.println("Manhattan distance: " + result);
     }
 
     public static void main(String[] args) {
@@ -52,59 +63,44 @@ public class Day3CrossedWires {
                 case 'U' -> {
                     for (int i = 0; i < length; i++) {
                         --x2;
-                        if (!isFirst && (grid[x2][y2] == '-' || grid[x2][y2] == '|' || grid[x2][y2] == '+')) {
-                            grid[x2][y2] = 'X';
-                            xf2 = x2;
-                            yf2 = y2;
-                        } else {
-                            grid[x2][y2] = '|';
-                        }
+                        updateGrid(isFirst, '|');
                     }
                     grid[x2][y2] = '+';
                 }
                 case 'D' -> {
                     for (int i = 0; i < length; i++) {
                         ++x2;
-                        if (!isFirst && (grid[x2][y2] == '-' || grid[x2][y2] == '|' || grid[x2][y2] == '+')) {
-                            grid[x2][y2] = 'X';
-                            xf2 = x2;
-                            yf2 = y2;
-                        } else {
-                            grid[x2][y2] = '|';
-                        }
+                        updateGrid(isFirst, '|');
                     }
                     grid[x2][y2] = '+';
                 }
                 case 'L' -> {
                     for (int i = 0; i < length; i++) {
                         --y2;
-                        if (!isFirst && (grid[x2][y2] == '-' || grid[x2][y2] == '|' || grid[x2][y2] == '+')) {
-                            grid[x2][y2] = 'X';
-                            xf2 = x2;
-                            yf2 = y2;
-                        } else {
-                            grid[x2][y2] = '-';
-                        }
+                        updateGrid(isFirst, '-');
                     }
                     grid[x2][y2] = '+';
                 }
                 case 'R' -> {
                     for (int i = 0; i < length; i++) {
                         ++y2;
-                        if (!isFirst && (grid[x2][y2] == '-' || grid[x2][y2] == '|' || grid[x2][y2] == '+')) {
-                            grid[x2][y2] = 'X';
-                            xf2 = x2;
-                            yf2 = y2;
-                        } else {
-                            grid[x2][y2] = '-';
-                        }
+                        updateGrid(isFirst, '-');
                     }
                     grid[x2][y2] = '+';
                 }
                 default -> throw new IllegalStateException("Unexpected value: " + instruction.charAt(0));
             }
         }
-        drawGrid();
+        //drawGrid();
+    }
+
+    private void updateGrid(boolean isFirst, char track) {
+        if (!isFirst && (grid[x2][y2] == '-' || grid[x2][y2] == '|' || grid[x2][y2] == '+')) {
+            grid[x2][y2] = 'X';
+            crossSections.add(new Point(x2, y2));
+        } else {
+            grid[x2][y2] = track;
+        }
     }
 
     private void drawGrid() {
