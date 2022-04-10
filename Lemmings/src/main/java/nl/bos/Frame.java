96b8d7036c956adeac7930f8ac08dd4a53494d91
hetbenkt@gmail.com <Abos@-02>
2022-04-10 10:37:25 +0200

    Shows Frame1 in black

--- Lemmings/src/main/java/nl/bos/Frame.java
+++ Lemmings/src/main/java/nl/bos/Frame.java
@@ -1,13 +1,25 @@
 package nl.bos;
 
 import javafx.scene.Parent;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 import java.util.List;
 
 public class Frame extends Parent {
+    private static final int PIXEL_SIZE = 10;
+    private static final int PIXEL_OFFSET = 12;
 
     public Frame(List<String> lines) {
-        this.getChildren().add(new Circle(10));
+        for (int i = 0; i < lines.size(); i++) {
+            String[] split = lines.get(i).split("");
+            for (int j = 0; j < split.length; j++) {
+                if (!split[j].equals("X")) {
+                    Rectangle rectangle = new Rectangle(PIXEL_SIZE, PIXEL_SIZE);
+                    rectangle.setTranslateX(j * PIXEL_OFFSET);
+                    rectangle.setTranslateY(i * PIXEL_OFFSET);
+                    this.getChildren().add(rectangle);
+                }
+            }
+        }
     }
 }
